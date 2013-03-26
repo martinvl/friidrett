7c35024cf6d83329a41e39ca8de3b85c52018303
Martin Vonheim Larsen <martinvl@student.matnat.uio.no>
2013-03-26 14:28:35 +0100

    Factored out DisciplineView

---
+++ view-tests/views/DisciplineSelectionView/DisciplineView.js
@@ -0,0 +1,62 @@
+var EventEmitter = require('events').EventEmitter;
+
+function DisciplineView() {
+    EventEmitter.prototype.constructor.apply(this);
+
+    this.initialize();
+    this.update();
+}
+
+DisciplineView.prototype = new EventEmitter();
+DisciplineView.prototype.constructor = DisciplineView;
+module.exports = DisciplineView;
+
+DisciplineView.prototype.initialize = function () {
+    this.setupState();
+    this.setupElement();
+};
+
+DisciplineView.prototype.setupState = function () {
+    this.state = {};
+};
+
+DisciplineView.prototype.setupElement = function () {
+    this.el = document.createElement('div');
+    this.el.className = 'discipline_view';
+
+    if (this.el.hasOwnProperty('ontouchend')) {
+        this.el.ontouchend = function () {
+            self.handleSelect();
+        };
+    } else {
+        this.el.onclick = function () {
+            self.handleSelect();
+        };
+    }
+
+    this.nameField = document.createElement('div');
+    this.nameField.className = 'discipline_name';
+    this.el.appendChild(this.nameField);
+};
+
+DisciplineView.prototype.setState = function (state, silent) {
+    this.state = state;
+
+    if (!silent)
+        this.update();
+};
+
+DisciplineView.prototype.handleSelect = function () {
+    this.emit('select');
+};
+
+DisciplineView.prototype.update = function () {
+    this.nameField.innerHTML = this.state.name;
+};
+
+/*
+   {
+   name:'',
+   description:''
+   }
+   */
