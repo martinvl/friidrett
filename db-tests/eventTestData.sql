91e1deb1361c506bb329e7cf39e9d64b02c1ebf5
Martin Vonheim Larsen <martinvl@student.matnat.uio.no>
2013-03-26 16:51:28 +0100

    Converted to UTF8

--- db-tests/eventTestData.sql
+++ db-tests/eventTestData.sql
@@ -1,9 +1,9 @@
-INSERT INTO Event VALUES (1, 'Spyd', 'MJ', 1, 'Søndre sving', '09:15:00', 'Ikke kast feil vei', 'True');
-INSERT INTO Event VALUES (2, 'Kule', 'MJ', 1, 'Nordre sving', '10:15:00', 'Ikke mist kula på tåa', 'True');
-INSERT INTO Event VALUES (3, 'Spyd', 'J-12', 1, 'Søndre sving', '09:45:00', 'Ikke kast feil vei', 'True');
-INSERT INTO Event VALUES (4, 'Kule', 'J-12', 1, 'Søndre sving', '10:45:00', 'Ikke mist kula på tåa', 'True');
-INSERT INTO Event VALUES (5, 'Spyd', 'MS', 1, 'Søndre sving', '12:15:00', 'Ikke kast feil vei', 'True');
-INSERT INTO Event VALUES (6, 'Spyd', 'G-12', 1, 'Søndre sving', '13:15:00', 'Ikke kast feil vei', 'True');
-INSERT INTO Event VALUES (7, 'Spyd', 'J-13', 1, 'Søndre sving', '14:15:00', 'Ikke kast feil vei', 'True');
-INSERT INTO Event VALUES (8, 'Spyd', 'KS', 1, 'Søndre sving', '15:15:00', 'Ikke kast feil vei', 'True');
-INSERT INTO Event VALUES (9, 'Spyd', 'KJ', 1, 'Søndre sving', '16:15:00', 'Ikke kast feil vei', 'True');
+INSERT INTO Event VALUES (1, 'Spyd', 'MJ', 1, 'SÃ¸ndre sving', '09:15:00', 'Ikke kast feil vei', 'True');
+INSERT INTO Event VALUES (2, 'Kule', 'MJ', 1, 'Nordre sving', '10:15:00', 'Ikke mist kula pÃ¥ tÃ¥a', 'True');
+INSERT INTO Event VALUES (3, 'Spyd', 'J-12', 1, 'SÃ¸ndre sving', '09:45:00', 'Ikke kast feil vei', 'True');
+INSERT INTO Event VALUES (4, 'Kule', 'J-12', 1, 'SÃ¸ndre sving', '10:45:00', 'Ikke mist kula pÃ¥ tÃ¥a', 'True');
+INSERT INTO Event VALUES (5, 'Spyd', 'MS', 1, 'SÃ¸ndre sving', '12:15:00', 'Ikke kast feil vei', 'True');
+INSERT INTO Event VALUES (6, 'Spyd', 'G-12', 1, 'SÃ¸ndre sving', '13:15:00', 'Ikke kast feil vei', 'True');
+INSERT INTO Event VALUES (7, 'Spyd', 'J-13', 1, 'SÃ¸ndre sving', '14:15:00', 'Ikke kast feil vei', 'True');
+INSERT INTO Event VALUES (8, 'Spyd', 'KS', 1, 'SÃ¸ndre sving', '15:15:00', 'Ikke kast feil vei', 'True');
+INSERT INTO Event VALUES (9, 'Spyd', 'KJ', 1, 'SÃ¸ndre sving', '16:15:00', 'Ikke kast feil vei', 'True');
