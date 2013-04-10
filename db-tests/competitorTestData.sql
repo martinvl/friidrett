38a46e53b285d8bf588db3f03fd6b65595473125
Mathias Johan Johansen <mathiasjjohansen@gmail.com>
2013-04-10 20:30:15 +0200

    Moar testdata

--- db-tests/competitorTestData.sql
+++ db-tests/competitorTestData.sql
@@ -3,3 +3,5 @@ INSERT INTO Competitor VALUES (2, 2, 'Martin V. Larsen', 'Moss IL', 'MJ', 1);
 INSERT INTO Competitor VALUES (3, 167, 'Sebastian Søberg', 'Moss IL', 'MJ', 1);
 INSERT INTO Competitor VALUES (4, 256, 'Ali Baba', 'IL Al-Jazeera', 'MS', 1);
 INSERT INTO Competitor VALUES (5, 123, 'Sami Ædda', 'Kautokeino IK', 'J-12', 1);
+INSERT INTO Competitor VALUES (6, 456, 'Ching Chong', 'IL Pling Plong', 'J-14', 1);
+INSERT INTO Competitor VALUES (7, 789, 'Mohammad Li', 'The Coopers', 'G-15', 1);
